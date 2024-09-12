48324ec1ad0eeb3d16716f34941d94d870af86e1
Kelly Brener-Raffalli <kbrener-@c1r2p3.42perpignan.fr>
2024-09-12 12:15:22 +0200

    faire_get_color_free_line

--- src/parsing.c
+++ src/parsing.c
@@ -40,10 +40,44 @@ int	check_openable(char *av)
 	fd = open(av, O_RDONLY);
 	return (fd);
 }
-
+int	get_path(char *line, char second_letter, int i, t_param *param)
+{
+	line++;
+	if (*line == second_letter)
+		line++;
+	if (*line != " ")
+		return (-1);
+	while (ft_strchr(" \t\n\v\r", *line))
+		line++;
+	if (!*line)//no texture's path
+		return (-1);
+	//0 = north, 1 = east, 2 = south, 3 = west
+	param->tex[i].path = ft_strdup(line);
+	param->format[i] = 1;
+	return (0);
+}
 int	keep_format(char *line, t_param *param)
 {
+	char	second_letter;
+	int		i;
 
+	second_letter = "O";
+	i = 0;
+	if (*line == "F" || *line == "C")
+		return (get_color(line, param));
+	else if (*line == "E")
+	{
+		second_letter = "A";
+		i++;
+	}
+	else if (*line == "S")
+		i = 2;
+	else if (*line == "W")
+	{
+		second_letter = "E";
+		i = 3;
+	}
+	return (get_path(line, second_letter, i, param));
 }
 
 /*check_format :
@@ -52,14 +86,11 @@ int	keep_format(char *line, t_param *param)
 	- swap lines with other things and spaces before the format description*/
 int	check_format(char *line, t_param *param)
 {
-	int	i;
-
-	i = 0;
-	while (ft_strchr(" \t\n\v\r", line[i]))
-		i++;
-	if (!line[i])
+	while (ft_strchr(" \t\n\v\r", *line))
+		line++;
+	if (!*line)
 		return (0);
-	if (line[i] && ft_strchr("NSWEFC", line[i]))
+	if (*line && ft_strchr("NSWEFC", *line))
 		return (keep_format(line, param));
 	else
 		return (1);
@@ -69,14 +100,17 @@ int	check_texture(t_param *param)
 	char	*line;
 	int		i;
 	int		map;
+	char	*save_line;
 
 	i = 0;
 	map = 0;
+	save_line = NULL;
 	line = get_next_line(param->fd);
 	if (!line)
 		return (-1);
 	while(line)
 	{
+		save_line = line;
 		map = check_format(line, param);
 		if (map == -1);//wrong format
 		{
